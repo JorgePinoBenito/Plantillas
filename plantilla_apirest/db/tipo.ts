0aa84d536878e8969184bd9369e8a9cd43ba6086
Jorge Pino Benito <jpinob@alumnos.nebrija.es>
2024-07-03 12:20:16 +0200

    final

--- plantilla_apirest/db/tipo.ts
+++ plantilla_apirest/db/tipo.ts
@@ -5,10 +5,15 @@ const Schema = mongoose.Schema;
 
 export type TipoModelType = mongoose.Document & Omit<Tipo, "_id">;
 export type TipoModelType = mongoose.Document & Tipo;
+export type TipoModelType = mongoose.Document &
+  Omit<Tipo, "_id"> & {
+    descripcion: string;
+  };
 
 const TipoSchema = new Schema({
   atributo: { type: String, required: true },
   nombres: { type: [String], required: true },
+  descripcion: { type: String, required: true },
 });
 
 export const TipoModel = mongoose.model<TipoModelType>("Tipo", TipoSchema);
--- plantilla_graphql/db/tipo.ts
+++ plantilla_graphql/db/tipo.ts
@@ -6,11 +6,12 @@ const Schema = mongoose.Schema;
 const TipoSchema = new Schema({
   atributo: { type: String, required: true },
   atributo2: { type: [String], required: true },
+  atributo3: { type: Number, required: true },
 });
 
 export type TipoModelType = mongoose.Document & Omit<Tipo, "id">;
 export type TipoModelType = mongoose.Document &
-  Omit<Tipo, "id"> & { atributo2: string };
+  Omit<Tipo, "id"> & { atributo3: number };
 export type TipoModelType = mongoose.Document & Tipo;
 
 export const TipoModel = mongoose.model<TipoModelType>("Tipo", TipoSchema);
